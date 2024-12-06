8767cb62eb2387b991f3b6b96af2a61b73271a5c
Ilya Roschikhmarov <ilrosch@mail.ru>
2024-12-06 16:23:13 +0300

    lesson 6: template pug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import fastify from 'fastify';
+import view from '@fastify/view';
+import pug from 'pug';
 
 const app = fastify();
 const port = 3000;
 
+const state = {
+  courses: [
+    {
+      id: 1,
+      title: 'JS: Массивы',
+      description: 'Курс про массивы в JavaScript',
+    },
+    {
+      id: 2,
+      title: 'JS: Функции',
+      description: 'Курс про функции в JavaScript',
+    },
+  ],
+};
+
+await app.register(view, { engine: { pug } });
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.view('src/views/index');
 });
 
 app.get('/hello', (req, res) => {
@@ -12,8 +31,23 @@ app.get('/hello', (req, res) => {
   res.send(`Hello, ${firstName}!`);
 });
 
+app.get('/courses', (req, res) => {
+  const data = {
+    courses: state.courses,
+    header: 'Курсы по программированию',
+  };
+  res.view('src/views/courses/index', data);
+});
+
 app.get('/courses/:id', (req, res) => {
-  res.send(`Course ID: ${req.params.id}`);
+  const { id } = req.params;
+  const course = state.courses.find(({ id: courseId }) => courseId === parseInt(id));
+  if (!course) {
+    res.code(404).send({ message: 'Course not found' });
+    return;
+  }
+  const data = { course };
+  res.view('src/views/courses/show', data);
 });
 
 app.get('/users', (req, res) => {

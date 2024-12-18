b72da3374372917c897e725bafbe6e694ff441b2
Ilya Roschikhmarov <ilrosch@mail.ru>
2024-12-18 21:58:54 +0300

    update port apps

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 import getApp from "../src/index.js";
 
 const app = await getApp();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
